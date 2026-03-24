f3ea95c1a108110502e9f8d15da96e397a6d6555
galiihajiip <gajipgaming@gmail.com>
2026-03-24 21:04:45 +0700

    chore: auto-save [2026-03-24 21:04:45]

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [],   // add domains if using external images
+    unoptimized: false,
+  },
 };
 
 export default nextConfig;
