3ebac6d83b87c71b434af6876483930b581ab1cb
galiihajiip <gajipgaming@gmail.com>
2026-03-24 18:54:06 +0700

    chore: auto-save [2026-03-24 18:54:06]

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 import InputField from '@/components/InputField'
 import LensSelector from '@/components/LensSelector'
 import ResultCard from '@/components/ResultCard'
